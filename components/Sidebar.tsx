aab0edae6cbf15d13c0575a77d68b70b13f6668c
daus <rivakanzf@gmail.com>
2023-12-17 16:00:40 +0700

    added sidebar icon

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react'
-import { Layout, Menu } from 'antd'
+import { Image, Layout, Menu } from 'antd'
 import { UploadOutlined, UserOutlined, VideoCameraOutlined, MenuOutlined } from '@ant-design/icons';
 
 const  { Sider } = Layout
@@ -14,45 +14,62 @@ const Sidebar = () => {
     //       label: `nav ${index + 1}`,
     //     }),
     //   );
+
     const items = [
         {
             key: 1,
-            icon: React.createElement(UserOutlined),
+            icon: React.createElement(() => (
+                <Image width={36} className='pr-4' src='/user.png'/>
+            )),
             label: 'User List'
         },
         {
             key: 2,
-            icon: React.createElement(UserOutlined),
+            icon: React.createElement(() => (
+                <Image width={36} className='pr-4' src='/driver.png'/>
+            )),
             label: 'Driver List'
         },
         {
             key: 3,
-            icon: React.createElement(UserOutlined),
+            icon: React.createElement(() => (
+                <Image width={36} className='pr-4' src='/route.png'/>
+            )),
             label: 'Route List'
         },
         {
             key: 4,
-            icon: React.createElement(UserOutlined),
+            icon: React.createElement(() => (
+                <Image width={36} className='pr-4' src='/ticket.png'/>
+            )),
             label: 'Voucher Catalogue'
         },
         {
             key: 5,
-            icon: React.createElement(UserOutlined),
+            icon: React.createElement(() => (
+                <Image width={36} className='pr-4' src='/vehicle.png'/>
+            )),
             label: 'Trip List'
         },
         {
             key: 6,
-            icon: React.createElement(UserOutlined),
+            icon: React.createElement(() => (
+                <Image width={36} className='pr-4' src='/clipboard-list.png'/>
+            )),
             label: 'Booking List'
         },
         {
             key: 7,
-            icon: React.createElement(UserOutlined),
+            icon: React.createElement(() => (
+                <Image width={36} className='pr-4' src='/cash.png'/>
+            )),
             label: 'All Transaction'
         },
         {
             key: 8,
-            icon: React.createElement(UserOutlined),
+            icon: React.createElement(() => (
+                <Image width={36} className='pr-4' src='/document-text.png'/>
+            )),
             label: 'Ride List'
         },
     ]
@@ -69,7 +86,7 @@ const Sidebar = () => {
                 console.log(collapsed, type);
                 }}
             >
-                <MenuOutlined className='pl-7 my-4' />
+                <MenuOutlined className='pl-8 my-4' />
                 <div className="demo-logo-vertical" />
                 <Menu theme="light" mode="inline" defaultSelectedKeys={['1']} items={items} />
             </Sider>
